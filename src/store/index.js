ef75b9b3b33013e541994c8d8dfd577c6585d3d5
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-02-05 11:20:07 +0100

    edited

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import artistsReducer from "../reducers/artists";
+import albumsReducer from "../reducers/albums";
+import likesReducer from "../reducers/likes";
+import usersReducer from "../reducers/users";
+import thunk from "redux-thunk";
+
+const composedEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {
+  likes: [],
+  artists: [],
+  album: {
+    currentSong: {},
+    albums: [],
+  },
+
+  user: {
+    username: "",
+    password: "",
+    email: "",
+  },
+};
+
+const allReducers = combineReducers({
+  likes: likesReducer,
+  artists: artistsReducer,
+  album: albumsReducer,
+  user: usersReducer,
+});
+
+export default function configureStore() {
+  return createStore(
+    allReducers,
+    initialState,
+    composedEnhancer(applyMiddleware(thunk))
+  );
+}
